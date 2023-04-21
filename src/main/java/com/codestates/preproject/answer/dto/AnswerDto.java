31e11889bcda19e2f10c5ca0985337aaffed3477
seoeunjeong <2juillet.acc@gmail.com>
2023-04-21 13:28:05 +0900

    [refactor] 매핑

--- src/main/java/com/codestates/preproject/answer/dto/AnswerDto.java
+++ src/main/java/com/codestates/preproject/answer/dto/AnswerDto.java
@@ -7,7 +7,6 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class AnswerDto {
