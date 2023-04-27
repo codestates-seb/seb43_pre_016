8627707fd7a95171545866424c361815e398bdc8
YujunSun0 <cons202@gmail.com>
2023-04-27 18:29:22 +0900

    빌드 수정

--- client/src/components/questions/Questions.js
+++ client/src/components/questions/Questions.js
@@ -240,8 +240,7 @@ const Questions = ({ cookies }) => {
       .get(
         `${process.env.REACT_APP_API_URL}/questions${
           "?page=" + currentpage + "&size=15"
-        }`,
-        header
+        }`
       )
       .then((res) => {
         setListData([...res.data.data]);
